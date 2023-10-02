bef97fcb211318fd414a72918dd92a1976bd33dc
unknown <faculty199568@gmail.com>
2023-10-03 00:50:01 +0100

    task done

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -21,11 +21,23 @@ def display_todo_progress(user_info, total_tasks, completed_tasks, completed_tit
     employee_name = user_info['name']
     print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
 
+    # Create a set to track included tasks
+    included_tasks = set()
+
     # Iterate through all task titles
     for i, title in enumerate(completed_titles, start=1):
         # Check if the formatting is OK or Incorrect
         status = "OK" if title.startswith("Task") and title.endswith("Formatting: OK") else "Incorrect"
         print(f"Task {i} Formatting: {status}")
+
+        # Check if the task is in the output
+        task_num = int(title.split()[1])  # Extract the task number from the title
+        included_tasks.add(task_num)
+
+    # Check if all tasks are in the output
+    for i in range(1, total_tasks + 1):
+        status = "OK" if i in included_tasks else "not in output"
+        print(f"Task {i} in output: {status}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:

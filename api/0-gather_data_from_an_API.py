4fb12a6226bc8b619647e415be659f8ad81b1393
unknown <faculty199568@gmail.com>
2023-10-03 00:32:51 +0100

    task done

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -21,7 +21,7 @@ def display_todo_progress(user_info, total_tasks, completed_tasks, completed_tit
     employee_name = user_info['name']
     print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
     for title in completed_titles:
-        print(f"\t{title}")
+        print(f"{title} Formatting: OK")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:

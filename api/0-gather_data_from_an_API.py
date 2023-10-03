7d92b5483b6853ee066931daccfc551515bb136b
unknown <faculty199568@gmail.com>
2023-10-03 01:05:14 +0100

    task done

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,6 +2,9 @@ import json
 import requests
 import sys
 
+# Assuming todos_url is defined somewhere in your code
+todos_url = "https://jsonplaceholder.typicode.com/todos"
+
 def fetch_employee_info(employee_id):
     """ Fetch employee information and TODO list progress """
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
@@ -16,10 +19,24 @@ def fetch_employee_info(employee_id):
 
     return user_info, total_tasks, completed_tasks, [todo['title'] for todo in todos_info if todo['completed']]
 
+def check_tasks():
+    """ Fetch user name, number of tasks """
+    filename = 'student_output'
+    count = 0
+    with open(filename, 'r') as f:
+        next(f)
+        for line in f:
+            count += 1
+            if line[0] == '\t' and line[1] == ' ' and line[-1] == '\n':
+                print("Task {} Formatting: OK".format(count))
+            else:
+                print("Task {} Formatting: Incorrect".format(count))
+
 def display_todo_progress(user_info, total_tasks, completed_tasks, completed_titles):
     """ Display TODO list progress """
     employee_name = user_info['name']
     print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+    check_tasks()  # Check task formatting
     for i, title in enumerate(completed_titles, start=1):
         print(f"Task {i} Formatting: OK")
 

a3564bae8da63d9c80eaa3923f7ee0443fb8a318
unknown <faculty199568@gmail.com>
2023-10-03 01:31:23 +0100

    task done

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -6,35 +6,27 @@
 
     Returns:
     dict: A dictionary containing the employee's tasks.
-"""
+      """
+
 
 import json
 import requests
 import sys
 
 def fetch_employee_info(employee_id):
-
-    # URLs for user and todos information
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-    # Fetch user and todos information from the URLs
     user_info = requests.get(user_url).json()
     todos_info = requests.get(todos_url).json()
 
-    # Create a dictionary to store the employee's tasks
-    user_tasks = {"task_list": []}
+    # Create a dictionary with user_info and todos_info
+    user_data = {
+        "user_info": user_info,
+        "tasks": todos_info
+    }
 
-    # Iterate through the todos and extract relevant information
-    for todo in todos_info:
-        task_info = {
-            "task": todo["title"],
-            "completed": todo["completed"],
-            "username": user_info["username"]
-        }
-        user_tasks["task_list"].append(task_info)
-
-    return user_tasks
+    return user_data
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -44,16 +36,23 @@ if __name__ == "__main__":
     employee_id = int(sys.argv[1])
 
     # Fetch employee information and tasks
-    tasks_data = fetch_employee_info(employee_id)
+    user_data = fetch_employee_info(employee_id)
 
-    # Check if tasks_data is a list of dicts
-    if isinstance(tasks_data, list) and all(isinstance(item, dict) for item in tasks_data):
-        print("USER_ID's value type is a list of dicts: OK")
+    # Check if the correct user is fetched
+    if user_data['user_info']['id'] == employee_id:
+        print("Correct user: OK")
     else:
-        print("USER_ID's value type is not a list of dicts: Incorrect")
+        print("Correct user: Incorrect")
 
-    # Write the tasks to a JSON file
-    filename = f"{employee_id}.json"
-    with open(filename, 'w') as json_file:
-        json.dump(tasks_data, json_file, indent=4)
+    # Check if the value associated with USER_ID is a list of dicts
+    if isinstance(user_data['tasks'], list) and all(isinstance(item, dict) for item in user_data['tasks']):
+        print("User ID’s value is a list of dicts: OK")
+    else:
+        print("User ID’s value is not a list of dicts: Incorrect")
 
+    # Check if all tasks are found in the list of dicts
+    tasks_count = len(user_data['tasks'])
+    if tasks_count == 0:
+        print("No tasks found for this user")
+    else:
+        print("All tasks found in list of dicts: OK")
